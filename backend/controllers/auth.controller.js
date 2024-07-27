02e2defc2e14f36e69fde965c5850398d4ca09ba
Neha20072007 <neha@valuebound.com>
2024-07-27 19:07:15 +0530

    1. project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req , res) => {
+        res.json({
+         data: "signup point",
+    });
+}
+
+export const login = async (req , res) => {
+    res.json({
+     data: "login point",
+});
+}
+
+export const logout = async (req , res) => {
+    res.json({
+     data: "logout point",
+});
+}
