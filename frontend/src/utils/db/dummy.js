84266e8d05e05cc9470e6ee49027e19f3c62e097
Neha20072007 <neha@valuebound.com>
2024-07-30 11:49:23 +0530

    3. auth and route protection

---
+++ frontend/src/utils/db/dummy.js
@@ -0,0 +1,116 @@
+export const POSTS = [
+	{
+		_id: "1",
+		text: "Chhappan bhog",
+		img: "/posts/post1.png",
+		user: {
+			username: "neha",
+			profileImg: "/avatars/girl1.png",
+			fullName: "neha",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "yum",
+				user: {
+					username: "neha",
+					profileImg: "/avatars/girl1.png",
+					fullName: "neha",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "2",
+		text: "How you guys doing? 😊",
+		user: {
+			username: "k",
+			profileImg: "/avatars/boy2.png",
+			fullName: "k",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "xyz",
+				user: {
+					username: "xyz",
+					profileImg: "/avatars/girl2.png",
+					fullName: "xyz",
+				},
+			},
+		],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894"],
+	},
+	{
+		_id: "3",
+		text: "Freedom",
+		img: "/posts/post2.png",
+		user: {
+			username: "johndoe",
+			profileImg: "/avatars/boy3.png",
+			fullName: "John Doe",
+		},
+		comments: [],
+		likes: ["6658s891", "6658s892", "6658s893", "6658s894", "6658s895", "6658s896"],
+	},
+	{
+		_id: "4",
+		text: "travelling",
+		img: "/posts/post3.png",
+		user: {
+			username: "fgh",
+			profileImg: "/avatars/boy3.png",
+			fullName: "Johg",
+		},
+		comments: [
+			{
+				_id: "1",
+				text: "Nice",
+				user: {
+					username: "akjdkjab",
+					profileImg: "/avatars/girl3.png",
+					fullName: "hgtytfyt",
+				},
+			},
+		],
+		likes: [
+			"6658s891",
+			"6658s892",
+			"6658s893",
+			"6658s894",
+			"6658s895",
+			"6658s896",
+			"6658s897",
+			"6658s898",
+			"6658s899",
+		],
+	},
+];
+
+export const USERS_FOR_RIGHT_PANEL = [
+	{
+		_id: "1",
+		fullName: "xyz",
+		username: "jsrd",
+		profileImg: "/avatars/boy2.png",
+	},
+	{
+		_id: "2",
+		fullName: "ohujv",
+		username: "knbhtx",
+		profileImg: "/avatars/girl1.png",
+	},
+	{
+		_id: "3",
+		fullName: "Bljmkh",
+		username: "bolmklmk",
+		profileImg: "/avatars/boy3.png",
+	},
+	{
+		_id: "4",
+		fullName: "Dalsgk",
+		username: "damsknerj",
+		profileImg: "/avatars/girl2.png",
+	},
+];
